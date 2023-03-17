22456a054a9fea833afc0bf8908ac4830d5dcb68
Dorota Zielinska <dorcius@protonmail.com>
2023-03-17 17:48:09 +0100

    wastebin icon added misspelling changed

--- js/script.js
+++ js/script.js
@@ -43,14 +43,15 @@
 
         for (const task of tasks) {
             htmlString += `
-        <li class="tasks__items js-task"> 
-        <button class="tasks__button tasks__button--toggleDone js-toggleDone">
-         ${task.done ? "✔" : ""}
-        </button>
-        <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
-         ${task.content}
-        </span>
-        <button class="tasks__button tasks__button--remove js-remove">⊠</button
+        <li class="tasks__items js-task"
+        > 
+         <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+          ${task.done ? "✔" : ""}
+         </button>
+         <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
+          ${task.content}
+         </span>
+         <button class="tasks__button tasks__button--remove js-remove"></button
         </li>
         `;
         }
@@ -71,7 +72,7 @@
             addNewTask(newTaskContent);
             newTaskElement.value = "";
         }
-        
+
         newTaskElement.focus();
     };
 
@@ -79,8 +80,8 @@
         render();
 
         const form = document.querySelector(".js-form");
-        form.addEventListener("submit", onFormSubmit);
 
+        form.addEventListener("submit", onFormSubmit);
     };
     init();
 }
